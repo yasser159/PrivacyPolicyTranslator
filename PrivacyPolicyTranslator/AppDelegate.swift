75cbdea7aa91608da94922f487b9242eed0e3087
Yasser Hajlaoui <yasser159@hotmail.com>
2022-01-14 15:33:13 -0800

    added api plist

--- PrivacyPolicyTranslator/AppDelegate.swift
+++ PrivacyPolicyTranslator/AppDelegate.swift
@@ -4,14 +4,18 @@
 //
 //  Created by Yasser Hajlaoui on 1/13/22.
 //
-
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
-
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        
+//        SwiftGoogleTranslate.shared.start(with: "API_KEY_HERE")
+//        
+//        return true
+//    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -24,7 +28,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
 
 }
 
--- PrivacyPolicyTranslator/ViewController.swift
+++ PrivacyPolicyTranslator/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: NSViewController {
     @IBOutlet weak var txt_Input: NSTextField!
     @IBOutlet weak var btn_generateFiles: NSButton!
     
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Secret", ofType: "plist") else {
+                    fatalError("Couldn't find file 'Config.plist'.")
+        }
+     
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API-Key") as? String else {
+                    fatalError("Couldn't find key 'API-Key' in 'Secret.plist'.")
+            }
+            return value
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -43,6 +57,8 @@ class ViewController: NSViewController {
         if let stringData = stringToSave.data(using: .utf8) {
             try? stringData.write(to: path)
         }
-       //print("File Saved: ", path)
+       print("File Saved: ", path)
+       // print("api key:", apiKey)
+        
     }
 }
