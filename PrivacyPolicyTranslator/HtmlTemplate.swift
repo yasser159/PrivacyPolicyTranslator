e02c82a69a32f8f383eddec1ecbb8a136432c350
Yasser Hajlaoui <yasser159@gmail.com>
2022-01-21 17:00:30 -0800

    fixed issue with the split eating the end of paragraph "</p>"

--- PrivacyPolicyTranslator/HtmlTemplate.swift
+++ PrivacyPolicyTranslator/HtmlTemplate.swift
@@ -45,33 +45,33 @@ func getHtml(languageCode: String, contentBody: String) -> String {
 
 let Html_testData = """
     <strong>Privacy Policy</strong>
-     <p>
+     <p>001
         Yasser Hajlaoui built the Covid Vaccine Passport app as
         a Free app. This SERVICE is provided by
         Yasser Hajlaoui at no cost and is intended for use as
         is.
-      </p> <p>
+      </p>002 <p>
         This page is used to inform visitors regarding my
         policies with the collection, use, and disclosure of Personal
         Information if anyone decided to use my Service.
-      </p> <p>
+      </p>003 <p>
         If you choose to use my Service, then you agree to
         the collection and use of information in relation to this
         policy. The Personal Information that I collect is
         used for providing and improving the Service. I will not use or share your information with
         anyone except as described in this Privacy Policy.
-      </p> <p>
+      </p>004 <p>
         The terms used in this Privacy Policy have the same meanings
         as in our Terms and Conditions, which are accessible at
         Covid Vaccine Passport unless otherwise defined in this Privacy Policy.
       </p> <p><strong>Information Collection and Use</strong></p>
-       <p>
+       <p>005
         For a better experience, while using our Service, I
         may require you to provide us with certain personally
         identifiable information. The information that
         I request will be retained on your device and is not collected by me in any way.
       </p> <!----> <p><strong>Log Data</strong></p>
-       <p>
+       <p>006
         I want to inform you that whenever you
         use my Service, in a case of an error in the app
         I collect data and information (through third-party
@@ -82,12 +82,12 @@ let Html_testData = """
         the time and date of your use of the Service, and other
         statistics.
       </p> <p><strong>Cookies</strong></p>
-       <p>
+       <p>007
         Cookies are files with a small amount of data that are
         commonly used as anonymous unique identifiers. These are sent
         to your browser from the websites that you visit and are
         stored on your device's internal memory.
-      </p> <p>
+      </p>008 <p>
         This Service does not use these “cookies” explicitly. However,
         the app may use third-party code and libraries that use
         “cookies” to collect information and improve their services.
@@ -97,20 +97,20 @@ let Html_testData = """
         portions of this Service.
       </p> <p><strong>Service Providers</strong>
     </p>
-       <p>
+       <p>009
         I may employ third-party companies and
         individuals due to the following reasons:
       </p> <ul><li>To facilitate our Service;</li>
         <li>To provide the Service on our behalf;</li> <li>To perform Service-related services; or</li>
          <li>To assist us in analyzing how our Service is used.</li></ul>
-         <p>
+         <p>010
         I want to inform users of this Service
         that these third parties have access to their Personal
         Information. The reason is to perform the tasks assigned to
         them on our behalf. However, they are obligated not to
         disclose or use the information for any other purpose.
       </p> <p><strong>Security</strong></p>
-       <p>
+       <p>011
         I value your trust in providing us your
         Personal Information, thus we are striving to use commercially
         acceptable means of protecting it. But remember that no method
@@ -118,7 +118,7 @@ let Html_testData = """
         storage is 100% secure and reliable, and I cannot
         guarantee its absolute security.
       </p> <p><strong>Links to Other Sites</strong></p>
-       <p>
+       <p>012
         This Service may contain links to other sites. If you click on
         a third-party link, you will be directed to that site. Note
         that these external sites are not operated by me.
@@ -128,8 +128,7 @@ let Html_testData = """
         privacy policies, or practices of any third-party sites or
         services.
       </p> <p><strong>Children’s Privacy</strong></p> <!---->
-      <div><p>
-          I do not knowingly collect personally
+      <div><p>013          I do not knowingly collect personally
           identifiable information from children. I
           encourage all children to never submit any personally
           identifiable information through
@@ -142,7 +141,7 @@ let Html_testData = """
           of your personally identifiable information in your country (in some countries we may allow your parent
           or guardian to do so on your behalf).
         </p></div> <p><strong>Changes to This Privacy Policy</strong></p>
-        <p>
+        <p>014
         I may update our Privacy Policy from
         time to time. Thus, you are advised to review this page
         periodically for any changes. I will
@@ -150,8 +149,8 @@ let Html_testData = """
         this page.
       </p> <p>This policy is effective as of 2022-01-11</p> <p><strong>Contact Us</strong>
     </p>
-      <p>
+      <p>015
         If you have any questions or suggestions about my
         Privacy Policy, do not hesitate to contact me at yasser159+CovidVaccinePassportApp@gmail.com
-      </p> <p>This privacy policy page was created at <a href="https://privacypolicytemplate.net" target="_blank" rel="noopener noreferrer">privacypolicytemplate.net </a>and modified/generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a></p>
+      </p> <p>This privacy policy page was created at <a href="https://privacypolicytemplate.net" target="_blank" rel="noopener noreferrer">privacypolicytemplate.net </a>and modified/generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a></p> 999
 """
--- PrivacyPolicyTranslator/ViewController.swift
+++ PrivacyPolicyTranslator/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: NSViewController {
     @IBOutlet weak var txt_input: NSTextField!
     @IBOutlet weak var txt_filePrefix: NSTextField!
     @IBOutlet weak var btn_generateFiles: NSButton!
+    @IBOutlet weak var chkbx_french: NSButtonCell!
+    @IBOutlet weak var progressBar: NSProgressIndicator!
     var languages = [String]()
     
     @IBOutlet weak var useTestData: NSButtonCell!
@@ -35,6 +37,8 @@ class ViewController: NSViewController {
     func checkIf_UsingTestData(){
         if useTestData.state == .on {
             txt_input.stringValue = Html_testData
+            //processCheckboxes(language: "fr", checkBoxState: NSControl.StateValue.on)
+            //chkbx_french.state = NSControl.StateValue.on
         }else {
             txt_input.stringValue = ""
         }
@@ -42,25 +46,31 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         checkIf_UsingTestData()
+        
     }
 
     @IBAction func CheckBoxManager(_ sender: NSButton) {
-
-    let  languageCode = dict_languageCodes[sender.title] ?? "en"
-        switch sender.state {
-        case .on:
-            languages.append(languageCode)
-        case .off:
-            while let idx = languages.firstIndex(of: languageCode) {
-                languages.remove(at: idx)
-            }
-        case .mixed:
-            print("check box was set to mixed")
-        default: break
-        }
+        processCheckboxes(language: sender.title, checkBoxState: sender.state)
     }
+    
+    func processCheckboxes(language: String, checkBoxState: NSControl.StateValue){
+        
+        let  languageCode = dict_languageCodes[language] ?? "en"
+        
+            switch checkBoxState {
+            case .on:
+                languages.append(languageCode)
+            case .off:
+                while let idx = languages.firstIndex(of: languageCode) {
+                    languages.remove(at: idx)
+                }
+            case .mixed:
+                print("check box was set to mixed")
+            default: break
+            }
+    }
+    
     
     override var representedObject: Any? {
         didSet {  // Update the view, if already loaded.
@@ -68,32 +78,72 @@ class ViewController: NSViewController {
     }
     
 @IBAction func GenerateFiles(_ sender: Any) {
-        
-    for languageCode in languages {
-        
-        if languageCode == "en" {
-            saveTranslationToFile(prefix: self.txt_filePrefix.stringValue, languageCode: languageCode, translatedText: txt_input.stringValue )
-        } else {
-            SwiftGoogleTranslate.shared.start(with: apiKey)
-            SwiftGoogleTranslate.shared.translate(txt_input.stringValue, languageCode, "en") { (text, error) in
-                if let translatedText = text {
-                    let filePrefix = self.txt_filePrefix.stringValue
-                    self.saveTranslationToFile(prefix: filePrefix, languageCode: languageCode, translatedText: translatedText)
-                }
-            }
+    SwiftGoogleTranslate.shared.start(with: apiKey)
+    
+    
+   for languageCode in languages {
+       
+               if languageCode == "en" {  //if english print and Exit, No translation needed
+                   saveTranslationToFile(prefix: self.txt_filePrefix.stringValue, languageCode: languageCode, translatedText: txt_input.stringValue )
+                   return
+               }
+       
+               var translatedTotal = ""
+               let inputText = txt_input.stringValue
+       
+               var components = inputText.components(separatedBy: "</p>") //Split The HTML Over </p> paraghraps
+               for (index, _) in components.enumerated() {
+                   if index < components.count - 1 {
+                       components[index].append("</p>")
+                   }
+               }
+       
+       
+       
+       
+              
+       let progressBar_Increment = Double(100/components.count)
+       
+              print("components.count:  ", components.count)
+              print("progressBar_Increment: ",progressBar_Increment)
+       
+               var progress = 0.0
+               progressBar.doubleValue = progress
+       
+               for paraghraph in components {
+                   SwiftGoogleTranslate.shared.translate(paraghraph, languageCode, "en") { (text, error) in
+                                                   if  let translatedText = text {
+                                                    translatedTotal.append(contentsOf: translatedText)
+                                                           //print(translatedText)
+                                                   }
+                                            }
+           
+                       //print(paraghraph)
+                       let second: Double = 1000000
+                       usleep(useconds_t(0.2 * second)) // Sleep for 200 milliseconds
+                   
+                       // Progress Bar Increments:
+                       progress += progressBar_Increment
+                       print(progress)
+                       self.progressBar.doubleValue = progress
+                       }
+       
+        //print(translatedTotal)
+       
+        let filePrefix = self.txt_filePrefix.stringValue
+        self.saveTranslationToFile(prefix: filePrefix, languageCode: languageCode, translatedText: translatedTotal)
+       
+       progress = 100.0
+       progressBar.doubleValue = progress
         }
-    }
 }
 
     func saveTranslationToFile(prefix: String, languageCode: String, translatedText: String ){
-        
         let myLanguage    = dict_languageCodes.someKey(forValue: languageCode) ?? "Default"
         let stringToSave  = getHtml(languageCode: languageCode, contentBody: translatedText)
         let fileName      = prefix + "-" + myLanguage + ".html"
         let path          = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
-        if let stringData = stringToSave.data(using: .utf8) {
-                                                                try? stringData.write(to: path)
-                                                            }
+        if let stringData = stringToSave.data(using: .utf8) { try? stringData.write(to: path)}
     }
     
 }// Last Bracket
